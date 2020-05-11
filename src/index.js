dd1643bb488917c16ecb4a86d7a59ded2b905ea7
Mary <mary.hopkins.a@gmail.com>
2020-05-11 09:56:47 -0400

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function component() {
+    const element = document.createElement('div');
+  
+    
+    element.innerHTML = "Hello world?";
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
